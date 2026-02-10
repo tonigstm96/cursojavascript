478dec768571e64220b5bbb01b5762e216f2eda8
ToniDev <tonigstm96@gmail.com>
2026-02-10 16:03:24 +0100

    Ejercicios operaciones a mitad

---
+++ Basic/05-operator-exercises.js
@@ -0,0 +1,48 @@
+/*
+Clase 20 - Ejercicios: Operadores
+Vídeo: https://youtu.be/1glVfFxj8a4?t=6458
+*/
+
+// 1. Crea una variable para cada operación aritmética
+let a = 1 + 1
+let b = 1 - 1
+let c = 1 * 1
+let d = 1 / 1
+let e = 1 % 1
+let f = 1 ** 1
+
+// 2. Crea una variable para cada tipo de operación de asignación,
+//    que haga uso de las variables utilizadas para las operaciones aritméticas
+
+let g = 1
+g += 1
+g -= 1
+g *= 1
+g /= 1
+g %= 1
+g **=1
+
+// 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+
+console.log(15 < 20)
+console.log(20 > 15)
+console.log(20 == "20")
+console.log(20 === 20)
+console.log(20 == 20 || 22 == 20)
+
+
+// 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+
+
+
+// 5. Utiliza el operador lógico and
+
+// 6. Utiliza el operador lógico or
+
+// 7. Combina ambos operadores lógicos
+
+// 8. Añade alguna negación
+
+// 9. Utiliza el operador ternario
+
+// 10. Combina operadores aritméticos, de comparáción y lógicas
