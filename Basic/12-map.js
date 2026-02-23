c6818c29065ee6b4d0b0fc0cb3dc49fce8b9797f
ToniDev <tonigstm96@gmail.com>
2026-02-23 18:08:18 +0100

    23/02 - 2

---
+++ Basic/12-map.js
@@ -0,0 +1,25 @@
+// Map
+
+// Declaración
+
+let myMap = new Map()
+
+console.log(myMap) 
+
+// Inicialización
+
+myMap = new Map([
+    ["name", "Toni"],
+    ["email", "tonigstm96@gmail.com"],
+    ["age", 29]
+])
+
+console.log(myMap) 
+
+// Métodos y propiedades
+
+// set
+
+myMap.set("alias", "ToniDev")
+myMap.set("name", "Toni Guti")
+console.log(myMap)
